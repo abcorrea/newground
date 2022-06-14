b736b94cf08be0e29fb5e411da991e79cc24552f
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2022-06-14 12:08:12 +0200

    Modify newground.sh to work with symbolic link on PATH

--- newground.sh
+++ newground.sh
@@ -1,1 +1,11 @@
-python3 $(dirname "$0")/newground.py $*
+#!/usr/bin/env bash
+
+# ATTENTION: We changed this script compared to the original repo. This now is
+# used simply as a symbolic link on the PATH to run the grounder from
+# everywhere. This is why we need to set up SCRIPT_DIR to point to the original
+# directory where the code is.
+
+
+SCRIPT_DIR="$(dirname $(readlink -f `which newground`))"
+
+python3 $SCRIPT_DIR/main.py $*
